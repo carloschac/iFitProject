ac79f85f4cab3b1d22d6a6ad2525e63e05d0aa2e
Carlos Henrique <carloss.chac@gmail.com>
2023-05-15 00:33:00 -0300

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,30 @@
+function handleMenu() {
+    const header = document.querySelector(".header");
+    const container = document.querySelector(".main-container");
+    const st = (window.pageYOffset || document.documentElement.scrollTop) >= 300;
+  
+    if (st) {
+      header.classList.add("header--active");
+      header.setAttribute("style", `width: ${container.clientWidth}px`);
+      container.setAttribute(
+        "style",
+        `padding-top: ${header.clientHeight + 50}px !important`
+      );
+    } else {
+      header.classList.remove("header--active");
+      header.removeAttribute("style", `width: ${container.clientWidth}px`);
+      container.removeAttribute(
+        "style",
+        `padding-top: ${header.clientHeight + 50}px !important`
+      );
+    }
+  }
+
+  window.addEventListener("scroll", handleMenu, false);
+window.addEventListener("resize", handleMenu, false);
+
 $(document).ready(function(){
+   
     $(".case-multiple-cards").slick({
     prevArrow: ".banner-slider__nav--prev",
     nextArrow: ".banner-slider__nav--next",
